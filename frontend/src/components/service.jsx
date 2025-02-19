0c4ead1f6bf2e5e45255d4f3e2e8a01b855c62ea
akashkrishnan2004 <akashkrishnan806@gmail.com>
2025-02-19 09:51:16 +0530

    Link add commit

--- frontend/src/components/service.jsx
+++ frontend/src/components/service.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
+
+const API_URL = "https://restaurant-application-backend-4ran.onrender.com/"
 
 export default API_URL
